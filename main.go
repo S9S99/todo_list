2642601fcf0dfa50ed600f21c8bd2f7607256e7a
Siroring <whiteloli99@gmail.com>
2024-03-07 00:04:24 +0900

    fix func name

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
   "github.com/gin-gonic/gin"
-  "net/http"
   "github.com/S9S99/todo_list/controllers"
 )
 
 func main() {
   router := gin.Default()
   router.LoadHTMLGlob("templates/*.tmpl")
-  router.GET("/", controllers.HomeGet)
+  router.GET("/", controllers.GetIndex)
 
   router.Run()
 }
